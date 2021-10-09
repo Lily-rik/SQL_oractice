235cc424c1687a4bf36b473dfff3e0be3f2599f0
reiko-ishihara <violet794.py16@gmail.com>
2021-10-09 23:49:00 +0900

    Chapter3

--- Chapter3.sql
+++ Chapter3.sql
@@ -36,13 +36,9 @@ Chapter3_practice
       WHERE 湿度 BETWEEN 60 AND 79
 
   12.-- SELECT * FROM 気象観測  -- 月がNULLのデータではなく、NULLデータのある月を表示する、文章の読み間違い
-      -- WHERE 月 IS NULL
+     --  WHERE 月 IS NULL
      SELECT 月 FROM 気象観測
       WHERE 降水量 IS NULL OR 最高気温 IS NULL OR 最低気温 IS NULL OR 湿度 IS NULL
-    
-
-
--- ここから解説見る続き！！！
 
 
 3-2
@@ -67,27 +63,39 @@ Chapter3_practice
 
   2.INSERT INTO 成績表
          VALUES('S001', '織田 信長', 77, 55, 80, 75, 93, NULL)
+		INSERT INTO 成績表		-- 一命令文ずつ記載が必要！
          VALUES('A002', '豊臣 秀吉', 64, 69, 70, 0, 59, NULL)
+		INSERT INTO 成績表		 
          VALUES('E003', '徳川 家康', 80, 83, 85, 90, 79, NULL)
   
-  4.UPDATE 成績表 SET 法学 = 85, 哲学 = 67
+  3.UPDATE 成績表 SET 法学 = 85, 哲学 = 67
      WHERE 学籍番号 = 'S001'
-    UPDATE 成績表 SET 外国語 = 81
-     WHERE 学籍番号 = 'A002' AND 'E003'
+  4.UPDATE 成績表 SET 外国語 = 81
+     -- WHERE 学籍番号 = 'A002' AND 'E003'   -- 自分の回答
+     WHERE 学籍番号 IN ('A002', 'E003')   -- 解答
 
-  5.(1)UPDATE 成績表 SET 総合成績 = 'A'
-        WHERE (法学 AND 経済学 AND 哲学 AND 情報理論 AND 外国語) > 80
+  5. -- ルールは(1)から順に適応されるため、(2)以降の記述には 総合成績 IS NULLの記述が必要になる
+    (1)UPDATE 成績表 SET 総合成績 = 'A'
+        -- WHERE (法学 AND 経済学 AND 哲学 AND 情報理論 AND 外国語) > 80   -- 自分の回答
+       WHERE 法学 >= 80 AND 経済学 >= 80 AND 哲学 >= 80 AND 情報理論 >= 80 AND 外国語 >= 80  -- 解答：80以上なので論理演算子には=が必要
     (2)UPDATE 成績表 SET 総合成績 = 'B'
-        WHERE (法学 OR 外国語 > 80 ) AND (経済学 OR 哲学 >80)
+        -- WHERE (法学 OR 外国語 > 80 ) AND (経済学 OR 哲学 >80)   -- 自分の回答
+       WHERE (法学 >= 80 OR 外国語 >= 80 )
+         AND (経済学 >= 80 OR 哲学 >= 80)
+         AND 総合成績　IS  NULL   -- 解答：(1)の除外が必要なため
     (3)UPDATE 成績表 SET 総合成績 = 'C'
-        WHERE (法学 AND 経済学 AND 哲学 AND 情報理論 AND 外国語) < 50
+        -- WHERE (法学 AND 経済学 AND 哲学 AND 情報理論 AND 外国語) < 50   -- 自分の回答
+       WHERE 法学 < 50 AND 経済学 < 50 AND 哲学 < 50 AND 情報理論 < 50 AND 外国語 < 50
+         AND 総合成績 IS NULL
     (4)分からない
+      -- (1)〜(3)に当てはまらないものは総合成績がNULLになっている
+      UPDATE 成績表 SET 総合成績 = 'C'
+       WHERE 総合成績 IS NULL
+   6.DELETE FROM 成績表
+      WHERE 法学 = 0 OR 経済学 = 0 OR 哲学 = 0 OR 情報理論 = 0 OR 外国語 = 0
 
 
 3-4
-  1.ない
+  1.ない		-- 解答：月(NULLではなく、月が被ることはないため)
   2.コード
   3.学籍番号
-
-
-  
